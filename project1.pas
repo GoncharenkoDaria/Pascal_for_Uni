d423c4d83065fc1bb962999c96cdb46f6f5050fe
Дарья Д. Гончаренко <96291902+GoncharenkoDaria@users.noreply.github.com>
2024-04-16 22:16:53 +0300

    1

--- project1.pas
+++ project1.pas
@@ -1,45 +1,64 @@
-var n,i,j,u:integer;  {сторона кв. матрицы, координаты элемента, счетчик}
-a,b:array[1..50,1..50] {матрицы начальная и измененная} of integer;
+var
+Sn, Sn1: double;
+Sum, sum1, N: double;
+eps: real;
+abserr, relerr: double;
+X, Y: double;
+fact: double;
+i,k:integer;
+{вводим погрешность и точку х}
+begin
+writeln('Enter accuracy (eps>0)');
+readln(eps);
+writeln('Enter point');
+readln(X);
+{Вывод таблицы}
+writeln('Member #': 25, 'Value of the member': 25, 'Partial sum': 25);
+for i := 1 to 75 do
+Write('-');
+writeln;
+
+Sum:=1;
+N:=2;
+k:=-1;
+Y:=1;
+i:=1;
+while abs(Y) > eps do
 begin
 
-write('n=');readln(n);
-writeln('original matrix:');
-randomize;
-u:=0;
-for i:=1 to n do
-  begin
-    for j:=1 to n do
-      begin
-        a[i,j]:=random(2);{заполнение нулями и единицами}
-        write(a[i,j]:4);
-      end;
-  writeln;
-  end;
- Writeln('Turned by 90 degrees anti-clockwise:') ;
-repeat   {повторение 3 раза}
-  u:=u+1;
-for i:=1 to n do
-for j:=1 to n do
-b[i,j]:=a[n-j+1,i];{поворот}
 
-for i:=1 to n do
-  begin
-    for j:=1 to n do
-      begin
-       a[i,j]:=b[i,j] ;  {замена исходной матрицы}
-      end;
+Y:=(Y*(X*X))/(N*(N-1));
+if (k=(-1)) then
+Sum:=sum-Y
+else   Sum:=sum+Y;
+N:=N+2;
+k:=(-1)*k;
 
-  end;
-   until u=3 ;
+writeln (i:25, Y:25:5,sum:25:5);
+ i:=i+1
+ end;
+
+ writeln (Sum);
 
 
-for i:=1 to n do
-  begin
-    for j:=1 to n do
-      begin
-        write(b[i,j]:3);    {вывод результата}
-      end;
-  writeln;
-  end ;
-readln;
+
+
+
+
+writeln('Approximated function value : ', sum: 0: 20);
+
+sum1 := cos(X);
+
+writeln('Exact functon value: ', sum1: 0: 20);
+
+abserr := abs(sum1 - sum);
+
+writeln('Absolute error: ', abserr: 0: 20);
+
+relerr := abs((sum1 - sum) / sum1);
+
+writeln('Relative error: ', relerr: 0: 20, ' percent.');
+
+readln();
+
 end.
